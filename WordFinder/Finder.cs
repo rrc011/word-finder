3b53c284bf77eb5f2505a9be3a6fe5cc154af7e7
Rafael Rodriguez <rafaelrodriguez111294@gmail.com>
2024-01-02 16:17:03 -0400

    init

---
+++ WordFinder/Finder.cs
@@ -0,0 +1,88 @@
+﻿namespace WordFinder
+{
+    public class Finder
+    {
+        private readonly IEnumerable<string> _grid;
+
+        // Constructor that initializes the grid with input data
+        public Finder(IEnumerable<string> input)
+        {
+            _grid = input;
+        }
+
+        // Method to find words from a wordstream in the grid
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            // Extract all words from the grid
+            var wordsInMatrixHorizontal = ExtractHorizontalWordsFromMatrix(_grid);
+            var wordsInMatrixVertical = ExtractVerticalWordsFromMatrix(_grid);
+
+            // Dictionary to store word frequency
+            var wordFrequency = new Dictionary<string, int>();
+
+            // Loop through each word in the wordstream
+            foreach (var word in wordstream)
+            {
+                // Count occurrences of the word in the grid
+                int countHorizontal = CountOccurrences(word, wordsInMatrixHorizontal);
+                int countVertical = CountOccurrences(word, wordsInMatrixVertical);
+
+                wordFrequency[word] = countHorizontal + countVertical; // Store word frequency
+            }
+
+            // Get top N words based on frequency
+            return GetTopNWords(wordFrequency, 10);
+        }
+
+        // Extracts words from the matrix/grid
+        private List<string> ExtractHorizontalWordsFromMatrix(IEnumerable<string> matrix)
+        {
+            var words = new List<string>();
+            foreach (var row in matrix)
+            {
+                var rowWords = row.Replace(",", "");
+                words.Add(rowWords);
+            }
+            return words;
+        }
+
+        private List<string> ExtractVerticalWordsFromMatrix(IEnumerable<string> matrix)
+        {
+            var verticalWords = new List<string>();
+
+            // Iterates through each column of the matrix
+            for (int i = 0; i < matrix.First().Length; i++)
+            {
+                string verticalWord = "";
+
+                // Iterates through each row in the matrix
+                foreach (var row in matrix)
+                {
+                    //assign the first letter of each row
+                    verticalWord += row[i];
+                }
+
+                verticalWords.Add(verticalWord);
+            }
+
+            return verticalWords;
+        }
+
+
+        // Counts occurrences of a word in the grid
+        private int CountOccurrences(string word, List<string> wordsInMatrix)
+        {
+            var horizontalCount = wordsInMatrix.Count(w => w.Contains(word));
+            return horizontalCount;
+        }
+
+        // Get top N words based on their frequency
+        private List<string> GetTopNWords(Dictionary<string, int> wordFrequency, int n)
+        {
+            return wordFrequency.OrderByDescending(pair => pair.Value)
+                               .Take(n)
+                               .Select(pair => $"{pair.Key}: {pair.Value}")
+                               .ToList();
+        }
+    }
+}
---
+++ WordFinder/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace WordFinder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word Search!");
+
+            IEnumerable<string> matrix = new List<string>
+            {
+                "K,J,T,M,E,D,S,X,E,O,L,B,J,A,T",
+                "I,C,Y,Y,A,V,S,S,R,R,P,F,W,P,W",
+                "W,O,C,P,V,A,T,W,S,A,U,Q,A,P,B",
+                "I,U,A,E,A,R,R,X,Y,N,A,X,T,B,O",
+                "X,P,P,L,B,R,A,T,U,G,Z,M,E,A,X",
+                "W,L,R,I,L,G,W,U,H,E,S,I,R,N,S",
+                "W,L,R,I,L,G,W,U,H,E,S,I,R,A,S",
+                "J,O,I,O,A,V,B,B,K,X,M,R,M,N,H",
+                "C,G,C,A,C,E,E,X,H,M,R,D,E,A,X",
+                "W,S,O,L,K,J,R,U,D,E,Z,X,L,A,U",
+                "F,W,T,P,B,G,R,U,X,L,W,W,O,L,W",
+                "K,P,L,E,E,K,Y,V,K,O,N,Y,N,U,R",
+                "X,P,I,N,R,A,P,P,L,E,U,T,S,A,T",
+                "O,E,N,C,R,L,U,C,H,E,R,R,Y,U,E",
+                "U,W,C,K,Y,W,I,I,P,I,E,Z,X,M,U",
+                "N,D,B,S,N,Z,G,Y,Y,N,S,K,I,W,I"
+            };
+
+            var finder = new Finder(matrix);
+
+            Console.WriteLine("Input words to search separate by coma (,): ");
+            var wordToSearch = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(wordToSearch))
+            {
+                Console.WriteLine("You must enter at least one word to search. Please enter a word: ");
+                wordToSearch = Console.ReadLine();
+            }
+
+            var result = finder.Find(wordToSearch.Split(",").Select(x => x.ToUpper()).ToList());
+
+            Console.WriteLine("************RESULTS************");
+            foreach (var word in result)
+            {
+                Console.WriteLine($"{word}");
+            }
+        }
+    }
+
+}
